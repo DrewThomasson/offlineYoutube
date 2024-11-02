9df0de7a7e26e41b62b971496d497ea3d158b723
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-11-02 18:06:42 -0400

    Update setup.py idk hope this fixes it

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="offlineyoutube",
-    version="2.1.4",
+    version="2.1.5",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
