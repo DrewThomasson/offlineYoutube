c7d9c2428f1fef3880dbc3a876e2d0ad41a368b2
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-11-03 19:05:28 -0500

    Update setup.py changed to actually release version 1.2.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="offlineyoutube",
-    version="2.1.7b1",
+    version="2.1.7",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
