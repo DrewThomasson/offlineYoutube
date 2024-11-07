33581a1986d4db82e75248355f238645f5bd1d53
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-11-07 14:53:50 -0500

    Update setup.py made it a b1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.system() == "Darwin" and platform.processor() == "arm":
 
 setup(
     name="offlineyoutube",
-    version="2.2.0b",
+    version="2.2.0b1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
